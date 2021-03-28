51a424e577236f51a11f9ee04b9fc21ac37733fa
Zach <zab5957@rit.edu>
2021-03-28 18:12:38 -0400

    project updates for fritzing diagram and images

--- project2/project2.ino
+++ project2/project2.ino
@@ -23,7 +23,7 @@ const int ledPinGreen = 5;
 const int ledPinWhite = 4; 
 
 /* Analog Pins */
-const int knockSensor = A2;
+const int buttonPin = A2;    
 const int mic = A4; 
 
 const int threshold = 70;  // threshold value for detected vibration is a knock
@@ -31,6 +31,9 @@ const int threshold = 70;  // threshold value for detected vibration is a knock
 unsigned long lastKnockTime = 0;  // the last time the knock sensor was read
 unsigned long knockDelay = 10;    // time between knock sensor readings
 
+
+// variables will change:
+int buttonState = 0;         // variable for reading the pushbutton status
 int sensorReading = 0;      // variable to store the value read from the sensor pin
 
 int ledState = LOW;         // variable used to store the last LED status, to toggle the light
@@ -49,27 +52,47 @@ void loop() {
   // read the mic and always send it to computer:
   Serial.print("Mic Level "); 
   Serial.println(analogRead(mic));
-
+  
   sensorReading = analogRead(mic);
+
+  //read the button state 
+  buttonState = digitalRead(buttonPin);
+
 
   // If it is time, read knock sensor and store it:
   if (millis() - lastKnockTime > knockDelay) {
     lastKnockTime = millis();
-    sensorReading = analogRead(knockSensor);
+    sensorReading = analogRead(buttonPin);
 
     
     /* if (sensorReading > 20) { //half a knock? noise?
       Serial.println(sensorReading);
     }*/
 
+      // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
+        if (buttonState == HIGH) {
+              if (sensorReading){
+          // turn LED on:
+          digitalWrite(ledPinRed, HIGH);
+          digitalWrite(ledPinGreen, HIGH);          
+          digitalWrite(ledPinWhite, HIGH);    
+              }
+          } 
+        else {
+          // turn LED off:
+          digitalWrite(ledPinRed, LOW);
+          digitalWrite(ledPinGreen, LOW);          
+          digitalWrite(ledPinWhite, LOW);    
+        }
+
     // if the sensor reading at the threshold:
     if (sensorReading >= threshold) {
       // toggle the status of the ledPin:
       ledState = !ledState;
       // update the LED pin itself:
-      digitalWrite(ledPin, ledState);
+      digitalWrite(ledPinRed, ledState);
       // send the string "Knock!" to the computer
-      Serial.println("Knock!");
+      Serial.println("Button Pressed!");
       // delay even more to avoid knock bounciness
       lastKnockTime = lastKnockTime + 60;
     }
