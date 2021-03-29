2ebf492a94b0d67731f7db7a6e614e6cb74d94fb
Zach <zab5957@rit.edu>
2021-03-28 22:46:58 -0400

    added green screenshot

--- project2/project2.ino
+++ project2/project2.ino
@@ -1,17 +1,14 @@
 /* LED connected to digital pins */
 const int ledPinRed = 3;     
 const int ledPinWhite = 4; 
-const int ledPinGreen = 5; 
+const int ledPinGreen = 2; 
 
 /* Analog Pins */
 const int buttonPin = 16;    
 const int mic = A3; 
 
-const int threshold = 70;  // threshold value for detected vibration is a knock
-
 //from knock sensor tutorial
 unsigned long lastSensorRead = 0;  // the last time the sensor was read
-
 
 // variables will change:
 int buttonState = 0;         // variable for reading the pushbutton status
@@ -35,40 +32,40 @@ void loop() {
   //read analog and digital sensors
   sensorReading = analogRead(mic);
   buttonState = digitalRead(buttonPin);
-          digitalWrite(ledPinGreen, HIGH);       
-   sensorReadOver = analogRead(sensorReading);
+  
+  sensorReadOver = analogRead(sensorReading);
     
   // read the mic and always send it to computer:
   Serial.print("Mic Level "); 
-
-    Serial.println(analogRead(sensorReading));
+ delayMicroseconds(2);
+  Serial.println(analogRead(sensorReading));
      
     if (buttonState == HIGH) {
         //check for noise
         if (sensorReadOver > 20 && sensorReadOver < 400) {
           delay(10);
           Serial.print("Red Low Noise"); 
-          Serial.println(analogRead(sensorReadOver));    
+          Serial.println(sensorReadOver);    
           // turn LED on:
- 
-          digitalWrite(ledPinGreen, HIGH);          
+          digitalWrite(ledPinRed, HIGH);
+          digitalWrite(ledPinGreen, LOW);          
           digitalWrite(ledPinWhite, LOW);  
         }
         //check for louder noise
         else if (sensorReadOver > 430 && sensorReadOver < 500) {
           delay(10);
           Serial.print("White some Noise");  
-          Serial.println(analogRead(sensorReadOver));    
+          Serial.println(sensorReadOver);    
           // turn LED on:
-
-          digitalWrite(ledPinGreen, HIGH);          
+          digitalWrite(ledPinWhite, HIGH);  
+          digitalWrite(ledPinGreen, LOW);          
           digitalWrite(ledPinRed, LOW);  
         }
          //check for super loud noise
          else if (sensorReadOver > 500) {
           // delay(10);      
           Serial.print("Green Loud!");  
-          Serial.println(analogRead(sensorReadOver)); 
+          Serial.println(sensorReadOver); 
           // turn LED on:
           digitalWrite(ledPinGreen, HIGH);
           digitalWrite(ledPinWhite, LOW);          
@@ -82,6 +79,6 @@ void loop() {
       digitalWrite(ledPinWhite, LOW);    
      }
 
-
+     
 
 }
